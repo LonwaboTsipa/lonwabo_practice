060858daadca18bb3332d18cfe744612b2e70fff
adrianriddle <adrian.riddle@kurtosys.com>
2017-06-19 15:10:41 +0200

    Null check on string processing

--- src/utils/processor.ts
+++ src/utils/processor.ts
@@ -53,7 +53,7 @@ export function getPropertyValue(instance: {}, property: IPropertyDescriptor | u
 
 	switch (property.dataType) {
 		case "STRG":
-			if (value) {
+			if (!isNullOrUndefined(value)) {
 				if (property["cardinality"] === "1") {
 					value = value.toString();
 				}
