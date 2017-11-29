1300c8f1ce8be6240a52ee2ca5887e3e4e0ecdcf
Kelly Truter <kellytruter@users.noreply.github.com>
2017-11-29 09:50:08 +0200

    Update start-staging.sh
    Disable properties run & change startup path to run form root of project

--- scripts/start-staging.sh
+++ scripts/start-staging.sh
@@ -1,7 +1,8 @@
 npm install
 npm run build
-npm run properties:staging
+# Enable Properties if needed (shouldn't run this if the clients are managing properties in fundpress)
+# npm run properties:staging
 
 pm2 stop "[PROJECT_CODE]_loader_staging"
 pm2 delete "[PROJECT_CODE]_loader_staging"
-export NODE_ENV=staging && pm2 start -f --name "[PROJECT_CODE]_loader_staging" "../dist/index.js" -- STAGING
+export NODE_ENV=staging && pm2 start -f --name "[PROJECT_CODE]_loader_staging" "dist/index.js" -- STAGING
