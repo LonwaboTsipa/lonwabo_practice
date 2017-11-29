d5c56b103103c546b90d2944528687dbe9cb43cd
Kelly Truter <kellytruter@users.noreply.github.com>
2017-11-29 09:51:09 +0200

    Update start-production.sh
    Startup script should run from the root of the project & properties disabled until needed

--- scripts/start-production.sh
+++ scripts/start-production.sh
@@ -1,7 +1,8 @@
 npm install
 npm run build
-npm run properties:staging
+# Enable Properties if needed (shouldn't run this if the clients are managing properties in fundpress)
+# npm run properties:staging
 
 pm2 stop "[PROJECT_CODE]_loader_prod"
 pm2 delete "[PROJECT_CODE]_loader_prod"
-export NODE_ENV=production && pm2 start -f --name "[PROJECT_CODE]_loader_prod" "../dist/index.js" -- PROD
+export NODE_ENV=production && pm2 start -f --name "[PROJECT_CODE]_loader_prod" "dist/index.js" -- PROD
